9241d65c3355b0930baaaf47ddb4a9ba966ec250
DavidHearl <david.hearl@outlook.com>
2024-10-08 17:09:42 +0100

    added csv file upload

---
+++ golf_data/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Data(models.Model):
+    source_name = models.CharField(max_length=100)
+    shot_number = models.IntegerField()
+    club = models.CharField(max_length=5)
+    strokes_gained = models.FloatField(null=True, blank=True)
+    carry_distance_range = models.FloatField()
+    carry_distance_premium = models.FloatField()
+    flat_carry_distance_range = models.FloatField()
+    flat_carry_distance_premium = models.FloatField()
+    total_distance_range = models.FloatField()
+    total_distance_premium = models.FloatField()
+    flat_total_distance_range = models.FloatField()
+    flat_total_distance_premium = models.FloatField()
+    apex_range = models.FloatField()
+    apex_premium = models.FloatField()
+    ball_speed_range = models.FloatField()
+    ball_speed_premium = models.FloatField()
+    launch_direction = models.CharField(max_length=5, null=True, blank=True)
+    launch_angle = models.FloatField()
+    from_center = models.CharField(max_length=5)
+    curve = models.CharField(max_length=5)
+    prox_to_hole = models.FloatField(null=True, blank=True)
+    target = models.CharField(max_length=5, null=True, blank=True)
+
+    def __str__(self):
+        return self.source_name
---
+++ golf_data/views.py
@@ -0,0 +1,50 @@
+import csv
+from django.shortcuts import render, redirect
+from .forms import UploadCSVForm
+from .models import Data
+
+
+def home(request):
+    shot_data = Data.objects.all()
+
+    # File upload logic
+    if request.method == 'POST':
+        form = UploadCSVForm(request.POST, request.FILES)
+        if form.is_valid():
+            csv_file = request.FILES['csv_file']
+            decoded_file = csv_file.read().decode('utf-8').splitlines()
+            reader = csv.DictReader(decoded_file)
+            for row in reader:
+                Data.objects.create(
+                    shot_number=int(row['Shot #']),
+                    club=row['Club'],
+                    strokes_gained=float(row['Strokes Gained']) if row['Strokes Gained'] != '--' else None,
+                    carry_distance_range=float(row['Carry Dist. Range (yd)']) if row['Carry Dist. Range (yd)'] != '--' else None,
+                    carry_distance_premium=float(row['Carry Dist. Premium (yd)']) if row['Carry Dist. Premium (yd)'] != '--' else None,
+                    flat_carry_distance_range=float(row['Flat Carry Dist. Range (yd)']) if row['Flat Carry Dist. Range (yd)'] != '--' else None,
+                    flat_carry_distance_premium=float(row['Flat Carry Dist. Premium (yd)']) if row['Flat Carry Dist. Premium (yd)'] != '--' else None,
+                    total_distance_range=float(row['Total Dist. Range (yd)']) if row['Total Dist. Range (yd)'] != '--' else None,
+                    total_distance_premium=float(row['Total Dist. Premium (yd)']) if row['Total Dist. Premium (yd)'] != '--' else None,
+                    flat_total_distance_range=float(row['Flat Total Dist. Range (yd)']) if row['Flat Total Dist. Range (yd)'] != '--' else None,
+                    flat_total_distance_premium=float(row['Flat Total Dist. Premium (yd)']) if row['Flat Total Dist. Premium (yd)'] != '--' else None,
+                    apex_range=float(row['Apex (ft)']) if row['Apex (ft)'] != '--' else None,
+                    apex_premium=float(row['Apex (ft) Premium']) if row['Apex (ft) Premium'] != '--' else None,
+                    ball_speed_range=float(row['Ball Speed (mph)']) if row['Ball Speed (mph)'] != '--' else None,
+                    ball_speed_premium=float(row['Ball Speed (mph) Premium']) if row['Ball Speed (mph) Premium'] != '--' else None,
+                    launch_direction=row['Launch Direction'].replace('°', '') if row['Launch Direction'] != '--' else None,
+                    launch_angle=float(row['Launch Angle (deg)'].replace('°', '')) if row['Launch Angle (deg)'] != '--' else None,
+                    from_center=row['From Centre (yd)'],
+                    curve=row['Curve'],
+                    prox_to_hole=float(row['Prox to Hole (yd)']) if row['Prox to Hole (yd)'] != '--' else None,
+                    target=row['Target (yd)']
+                )
+            return redirect('home')  # Redirect back to the home page
+    else:
+        form = UploadCSVForm()
+
+    context = {
+        'form': form,
+        'shot_data': shot_data
+    }
+        
+    return render(request, 'home.html', context)
